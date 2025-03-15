5f7c6256ee6c2432d4aac213cb59a7cd1257d8d8
DedDushnila <mishashuludyakov@gmail.com>
2025-03-15 23:42:49 +0300

    mod_113

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -20,7 +20,7 @@ public class UserServiceImpl implements UserService {
 
     public void saveUser(String name, String lastName, byte age) {
         userDaoJDBC.saveUser(name, lastName, age);
-        System.out.println("User " + name + " добавлен в БД");
+        System.out.println("User с именем - " + name + " добавлен в БД");
     }
 
     public void removeUserById(long id) {
